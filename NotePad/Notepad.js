7da6d372d6b0a8c94ebfec77ae20945fee3064f3
fiendgrin <yunisah@code.edu.az>
2023-07-24 03:33:26 +0400

    Note Creation logic done

--- NotePad/Notepad.js
+++ NotePad/Notepad.js
@@ -16,23 +16,23 @@ for (let Circle of Circles) {
 const NoteColors = [
   {
     id: 1,
-    botderColor: "rgba(240, 98, 146)",
+    borderColor: "rgba(240, 98, 146)",
     h4Color: "rgba(240, 98, 146, 0.4)",
   },
   {
     id: 2,
-    botderColor: "rgba(186, 104, 200)",
+    borderColor: "rgba(186, 104, 200)",
     h4Color: "rgba(186, 104, 200, 0.4)",
   },
-  { id: 3, botderColor: "rgba(79,195,247)", h4Color: "rgba(79,195,247, 0.4)" },
+  { id: 3, borderColor: "rgba(79,195,247)", h4Color: "rgba(79,195,247, 0.4)" },
   {
     id: 4,
-    botderColor: "rgba(255, 213, 79)",
+    borderColor: "rgba(255, 213, 79)",
     h4Color: "rgba(255, 213, 79, 0.4)",
   },
   {
     id: 5,
-    botderColor: "rgba(174, 213, 129)",
+    borderColor: "rgba(174, 213, 129)",
     h4Color: "rgba(174, 213, 129, 0.4)",
   },
 ];
@@ -40,10 +40,18 @@ const NoteColors = [
 const Save = document.getElementById("Save");
 const TitleInput = document.getElementById("TitleInput");
 const NoteText = document.getElementById("NoteText");
+const Cards = document.getElementById("Cards");
 
-
-
-
+function NoteMaker(id, borderColor, h4Color, h4Text, pText) {
+  return `<div style="border: 2px dashed ${borderColor};" CardId=${id} class="Card">
+          <h4 style="background-color: ${h4Color};" class="Cardh4">
+            ${h4Text} <img src="./Icons/Trash.svg" alt="" />
+          </h4>
+          <p>
+            ${pText}
+          </p>
+        </div>`;
+}
 
 Save.addEventListener("click", (e) => {
   // input checks
@@ -54,6 +62,8 @@ Save.addEventListener("click", (e) => {
     alert("Write a Note");
     return;
   }
+  let check = false;
+
   for (let Circle of Circles) {
     if (Circle.firstElementChild.style.display === "inline") {
       check = true;
@@ -65,4 +75,29 @@ Save.addEventListener("click", (e) => {
     return;
   }
   // Note Creation
+
+  let ColorId;
+  let ColorObj;
+
+  for (let Circle of Circles) {
+    if (Circle.firstElementChild.style.display === "inline") {
+      ColorId = Circle.id;
+    }
+  }
+
+  ColorObj = NoteColors.find((item) => item.id == ColorId);
+
+  Cards.innerHTML += NoteMaker(
+    ColorId,
+    ColorObj.borderColor,
+    ColorObj.h4Color,
+    TitleInput.value,
+    NoteText.value
+  );
+  TitleInput.value = "";
+  NoteText.value = "";
+
+  for (let Circle of Circles) {
+    Circle.firstElementChild.style.display = "none";
+  }
 });
